df83f24a850c945587f46fcc91469944bf5afb55
BT_3W2_08 <dbenyacar1@gmail.com>
2022-10-17 09:29:54 -0400

    End of class work

--- src/RightTriangle.java
+++ src/RightTriangle.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class RightTriangle {
+public class RightTriangle {
+    private double base;
+    private double height;
+
+    public RightTriangle(double base, double height){
+        this.base  = base;
+        this.height = height;
+    }
+
+    public double hypotenuse(){
+        double aSquared = Math.pow(base, 2);
+        double bSquared = Math.pow(height, 2);
+        double c = Math.pow((aSquared + bSquared), .5);
+        return c;
+    }
+
+    public String randomValues(){
+        double a = (int)(Math.random() * 35) + 51;
+        double b = Math.random() *9 + 1;
+        double c = Math.random() *75 + 25;
+        return "random 1: " + a + "\n" + "random 2: " + b + "\n" + "random 3: " + c + "\n";
+    }
 }
--- src/RightTriangleRunner.java
+++ src/RightTriangleRunner.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class RightTriangleRunner {
+public class RightTriangleRunner {
+    public static void main(String[] args){
+        RightTriangle triOne = new RightTriangle(5, 12);
+        RightTriangle triTwo = new RightTriangle(6, 8);
+        double hypOne = triOne.hypotenuse();
+        double hypTwo = triTwo.hypotenuse();
+        System.out.println(hypOne);
+        System.out.println(hypTwo);
+        System.out.println(triOne.randomValues());
+        for (int i = 0; i < 100; i++) {
+            int randomNum = (int)(Math.random() * 35) + 51;
+                    System.out.println(randomNum);
+        }
+
+    }
 }
